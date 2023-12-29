2d2c43de2ac7ffbcf29940e27d23e9c866901229
Aarthika08 <79691897+Aarthika08@users.noreply.github.com>
2023-12-29 12:36:55 +0530

    Update main.ts

--- topics/data binding/main.ts
+++ topics/data binding/main.ts
@@ -1,6 +1,6 @@
 // src/main.ts
 
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic'; 
 import { AppModule } from './app/app.module';
 
 platformBrowserDynamic().bootstrapModule(AppModule);
