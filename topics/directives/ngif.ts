4098cec7addf59349d81c32626e61a9c6e0fae85
Aarthika08 <79691897+Aarthika08@users.noreply.github.com>
2024-01-02 10:22:58 +0530

    Create ngif.ts

---
+++ topics/directives/ngif.ts
@@ -0,0 +1,41 @@
+// import { Component } from '@angular/core';
+
+// @Component({
+//   selector: 'app-root',
+//   // templateUrl: './app.component.html',
+
+//   template:`<div *ngIf="isUserLoggedIn; else loginmessage">
+//   welcome,user
+// </div>`,
+//   styleUrls: ['./app.component.css']
+// })
+// export class AppComponent {
+//   title = 'structures';
+//   isUserLoggedIn="True";
+// }
+
+
+// app.component.ts
+
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  template: `
+    <!-- <div *ngIf="isUserLoggedIn; else loginMessage">
+      Welcome, User!
+    </div>
+    <ng-template #loginMessage>
+      <div>
+        Please log in to access the content.
+      </div>
+    </ng-template> -->
+ 
+      
+  `,
+})
+export class AppComponent {
+  isUserLoggedIn = false; // Ensure this property is defined in your component
+items=['a','b','c'];
+// index=0;
+}
