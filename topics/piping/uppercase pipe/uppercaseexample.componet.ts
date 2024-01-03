b35d223c6634bfb36b6889cf14e4fa98aace606a
Aarthika08 <79691897+Aarthika08@users.noreply.github.com>
2024-01-03 12:24:27 +0530

    Create uppercaseexample.componet.ts

---
+++ topics/piping/uppercase pipe/uppercaseexample.componet.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-date-example',
+  templateUrl: './date-example.component.html',
+  styleUrls: ['./date-example.component.css']
+})
+export class DateExampleComponent {
+  text="Hello ! Viewer"
+}
