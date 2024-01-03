954914fb18c50c3406090305171c102c5d681a29
Aarthika08 <79691897+Aarthika08@users.noreply.github.com>
2024-01-03 12:43:47 +0530

    Update uppercaseexample.componet.ts

--- topics/piping/uppercase pipe/uppercaseexample.componet.ts
+++ topics/piping/uppercase pipe/uppercaseexample.componet.ts
@@ -2,9 +2,25 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-date-example',
-  templateUrl: './date-example.component.html',
-  styleUrls: ['./date-example.component.css']
+  templateUrl: './uppercaseexample.component.html',
+  styleUrls: ['./uppercaseexample.component.css']
 })
+
 export class DateExampleComponent {
+  currentDate: Date = new Date();
   text="Hello ! Viewer"
+
+  // pipe.component.ts
+birthday = new Date(2001, 8, 9);
+
+toggle = true;
+
+get format()
+{
+  return this.toggle ? 'mediumDate' : 'fullDate';
+}
+toggleFormat()
+  {
+    this.toggle = !this.toggle;
+  }
 }
