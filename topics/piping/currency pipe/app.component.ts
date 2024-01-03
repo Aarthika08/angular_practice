dec1068dc6813d3c0329c69e8d15a1a034e38bcd
Aarthika08 <79691897+Aarthika08@users.noreply.github.com>
2024-01-03 13:03:06 +0530

    Create app.component.ts

---
+++ topics/piping/currency pipe/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  price: number = 1234.5678;
+  selectedCurrency: string = 'USD';
+}
