dcc4b00e6c1a372bce403f6965866887feaa4926
Aarthika08 <79691897+Aarthika08@users.noreply.github.com>
2024-01-03 12:43:05 +0530

    Update lowercasepipe.component.ts

--- topics/piping/lowercase piping/lowercasepipe.component.ts
+++ topics/piping/lowercase piping/lowercasepipe.component.ts
@@ -5,6 +5,23 @@ import { Component } from '@angular/core';
   templateUrl: './lowercasepipe.component.html',
   styleUrls: ['./lowercasepipe.component.css']
 })
+
 export class DateExampleComponent {
+  currentDate: Date = new Date();
   text="Hello ! Viewer"
+
+  // pipe.component.ts
+birthday = new Date(2001, 8, 9);
+
+toggle = true;
+
+get format()
+{
+  return this.toggle ? 'mediumDate' : 'fullDate';
 }
+toggleFormat()
+  {
+    this.toggle = !this.toggle;
+  }
+}
+
