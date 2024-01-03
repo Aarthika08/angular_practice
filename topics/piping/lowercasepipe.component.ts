6ecae8cd7347321e0ee32ea2a41208a9dd31a6ad
Aarthika08 <79691897+Aarthika08@users.noreply.github.com>
2024-01-03 12:38:03 +0530

    Create lowercasepipe.component.ts

---
+++ topics/piping/lowercasepipe.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-date-example',
+  templateUrl: './lowercasepipe.component.html',
+  styleUrls: ['./lowercasepipe.component.css']
+})
+export class DateExampleComponent {
+  text="Hello ! Viewer"
+}
